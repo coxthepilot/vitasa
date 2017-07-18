d3d06c2e1ed24d59bd8dbbbaf2da925fa3a853da
Chris Cox <chriscox@abandonedfactory.net>
2017-07-18 11:02:08 -0700

    Apparently SnakeCase isn't acceptable for modules

--- lambdas/functions/GetSiteDetails.py
+++ lambdas/functions/GetSiteDetails.py
@@ -1,30 +1,37 @@
 import json
 import boto3
 import logging
-import urllib
+import urllib 
 
 from models.site import Site
-from .utilities import respond
+#from .utilities import respond
+def respond(statusCode, body):
+    return {
+        'statusCode': statusCode,
+        'body': body,
+        'headers': {
+            'Content-Type': 'application/json',
+        },
+    }
 
-class NoSuchAccountExistsException(Exception): pass
-class BadParameters(Exception): pass
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 def lambda_handler(event, context):
     #get the site name from the url
-    if 'params' not in event: 
-        respond(400, '{ "errorCode": "400", "errorMessage":"Bad Request: No Params found"}'
-    if 'path' not in event['params']: 
-        respond(400, '{ "errorCode": "400", "errorMessage":"Bad Request: No path found in Params"}'
-    if 'sitename' not in event['params']['path']: 
-        respond(400, '{ "errorCode": "400", "errorMessage":"Bad Request: No sitename found in Params.path"}'
+    if 'pathParameters' not in event:
+        return respond('400', '{ "errorCode": "400", "errorMessage":"Bad Request: No Site Slug specified in the path"}')
+    if 'sitename' not in event['pathParameters']:
+        return respond('400', '{ "errorCode": "400", "errorMessage":"Bad Request: No Site Slug found in the path parameters"}')
+    site_name = urllib.parse.unquote(event['pathParameters']['sitename'])
+
+    if len(site_name) == 0: 
+        return respond('400', '{ "errorCode": "400", "errorMessage":"Bad Request: Empty sitename"}')
         
-    quotedName = event['params']['path']['sitename']
-    iname = urllib.parse.unquote(quotedName)
-    if len(iname) == 0: 
-        respond(400, '{ "errorCode": "400", "errorMessage":"Bad Request: Empty sitename"}'
-        
-    site = Site.find(iname)
-    respond(200, site.to_json())
+    site = Site.find(site_name)
+
+    if site is None:
+        return respond('404', '{ "errorCode": "404", "errorMessage":"Invalid site name"}')
+
+    return respond('200', site.to_json())
---
+++ lambdas/tests/test_get_site_details.py
@@ -0,0 +1,28 @@
+import unittest
+import sys
+import json
+
+sys.path.insert(0, ".")
+from models.site import Site
+from functions.GetSiteDetails import *
+
+class TCLambdaSites(unittest.TestCase):
+    def test_get_site_details(self):
+        event = {
+            'httpMethod': 'GET',
+            'path': '/site/the-alamo',
+            'resource': '/site/{sitename}',
+            'pathParameters': {
+                'sitename': 'the-alamo'
+            },
+            'body': None,
+        }
+        response = lambda_handler(event, None)
+        self.assertIsNotNone(response)
+        self.assertEqual('200', response['statusCode'], 'Maybe the data hasn\'t been created yet? This is a manual process until we get fixtures installed')
+        site = Site.from_dict(json.loads(response['body']))
+        self.assertEqual('the-alamo', site.slug)
+        self.assertEqual('The Alamo', site.name)
+
+if __name__ == '__main__':
+    unittest.main()
