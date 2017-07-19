31d376e9b90edba0dcc06b1050d83fee344bcec1
Chris Cox <chriscox@abandonedfactory.net>
2017-07-18 19:28:35 -0700

    Updating makefile to use python's zip module

--- static/sites.js
+++ static/sites.js
@@ -1,14 +1,17 @@
 
-$.getJSON("https://s3-us-west-2.amazonaws.com/vita-static-content-dev/sites.json", function(sites) {
+$.getJSON("https://console.aws.amazon.com/s3/object/vitasa-static-content-dev/sites.json?crossorigin", function(sites) {
     console.log(sites);
     var template = $('#hidden-site-template').html();
     for (let site of sites) {
         var site_html = $(template).clone();
 
-        $(site_html).find('#address').html(site.address);
+        $(site_html).find('#xstreet').html(site.xstreet);
+        $(site_html).find('#xcit').html(site.xcity);
+        $(site_html).find('#xzip').html(site.xzip);
         $(site_html).find('#is_open').html(site.is_open.toString());
-        $(site_html).find('#availability_status').html(site.availability_status);
-        $(site_html).find('#hours').html(site.hours);
+        $(site_html).find('#days').html(site.days);
+        $(site_html).find('#opentime').html(site.opentime);
+        $(site_html).find('#closetime').html(site.closetime);
         
         $('#sites-list').append(site_html);
         
